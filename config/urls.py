d3fd3b36a48edbbf337f1ea0c00c08ec0c91b56d
User <tjasur@gmail.com>
2024-02-02 17:58:18 +0500

    a.2

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="E-commerce API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="tjasur224@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('products.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
---
+++ products/serializers.py
@@ -0,0 +1,25 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Product, Review, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    avg_rating = serializers.FloatField(read_only=True, required=False)
+    # read_only=True -> faqat o'qish uchun
+    # required=False -> to'ldirish majburiy emas
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'avg_rating']
