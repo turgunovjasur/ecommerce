094150771d17f5313ce0a959fe75d5965fb70889
User <tjasur@gmail.com>
2024-02-15 17:51:45 +0500

    a.8

--- products/urls.py
+++ products/urls.py
@@ -1,19 +1,24 @@
+from . import signals
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .services.flash_sale import FlashSaleListCreateView, check_flash_sale
 from .services.product_view_history import ProductViewHistoryCreate
-from .views import ProductViewSet, ReviewViewSet, CategoryViewSet
+from .services.replenish_stock import admin_replenish_stock
+from .views import ProductViewSet, ReviewViewSet, CategoryViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'reviews', ReviewViewSet)
 router.register(r'categories', CategoryViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
 
     path('sale/', FlashSaleListCreateView.as_view(), name='sale'),
     path('check-sale/<int:product_id>/', check_flash_sale, name='product-view-history-create'),
-    path('product-view/', ProductViewHistoryCreate.as_view(), name='product-view-history-create')
+    path('product-view/', ProductViewHistoryCreate.as_view(), name='product-view-history-create'),
+    path('admin/replenish_stock/<int:product_id>/<int:amount>', admin_replenish_stock, name='admin_replenish_stock'),
 ]
